958090e7a223cd2386a653560759b7b2a914ca68
capitjeff21 <36058298+capitjeff21@users.noreply.github.com>
2024-05-05 22:02:55 -0400

    Add SumZero

--- Python CodeGolf.py
+++ Python CodeGolf.py
@@ -2,7 +2,7 @@
 def findMaxK(nums: List[int]) -> int:
         return max(r) if (r := [i for i in nums if i>0 and i*-1 in [j for j in nums if j < 0]]) else -1
 
-# Leetcode Problem 1051. Wastes space by continuously resorting s, a two line solution would be more optimal (and it is what I have on Leetcode!) but for the purpose of codegolf it is one.
+# Leetcode Problem 1051. Wastes space by continuously resorting s, a two line solution would be more optimal (and it is what I have on Leetcode!) but for the purpose of CodeGolf it is one.
 # Checks how many heights are not in correct sorted order
 def heightChecker(self, heights: List[int]) -> int:
         return sum([1 for i in range(len(heights)) if heights[i] != sorted(heights)[i]])
@@ -11,3 +11,8 @@ def heightChecker(self, heights: List[int]) -> int:
 # Sort all cells in a Matrix of size [rows, cols] by Manhattan distance to [rCenter, cCenter]
 def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
         return sorted([[i, j] for i in range(rows) for j in range(cols)], key=lambda a : abs(a[0]-rCenter) + abs(a[1] - cCenter))
+
+# Leetcode Problem 1304. Could be so much more efficient but for the purpose of CodeGolf it works.
+# Create an array of size n with unique integers that sum to 0
+def sumZero(self, n: int) -> List[int]:
+        return [i for i in range(1, n//2+1)] + [-i for i in range(1, n//2+1)] + ([0] if n%2 else [])
